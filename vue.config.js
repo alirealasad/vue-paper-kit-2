6f1b98835a86a528b2eff4ceafc89ef94c3c199d
ASAD <alirealasad@gmail.com>
2020-11-18 00:28:19 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   pwa: {
     workboxPluginMode: 'InjectManifest',
-    name: 'vue-paper-kit-2'
+    name: 'vue-paper-kit-2',
+    workboxOptions:{
+      swSrc: "src/service-worker.js"
+   }
   }
 }
